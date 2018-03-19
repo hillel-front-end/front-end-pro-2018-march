7d4cdfa8e585713235b193868d0c883c96311c69
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-03-19 19:54:00 +0200

    L_2

---
+++ lections/lection_2_if-else_cycles/materials/main.js
@@ -0,0 +1,99 @@
+console.log("Lection_2");
+
+
+// boolean
+
+// true or false //  1 or 0
+
+// a = 5;
+// b = 10;
+
+// console.log( a > b );
+// console.log( a < b );
+// console.log( a >= b );
+
+// console.log( a <= b );
+
+// console.log('a == b' , a == b );
+
+// console.log('a != b' , a != b );
+// console.log('a === b' , a === b );
+
+//  // ---------------------------------
+
+//  //   
+//  console.log( 'b > 0 && b > a', b > 0 && b > a );
+
+//  a = 5;
+//  b = 10;
+//  console.log( !( !a === 0 && a > b) && ( b < 10 || a > 0) );
+
+// ---------------------------------
+
+// условный оператор if(){}
+
+// a = 10;
+
+// if (a > 0) {
+//     console.log('a > 0');
+
+//     // console.log('a > 0!!');
+
+//     // console.log('a > 0!!!!');
+// }
+
+// if ( a > 0 && a <= 10 ) {
+//     console.log('a > 0 && a <= 10');
+// }
+
+
+// console.log('Hello!');
+
+
+// ---------------------------------
+
+// a = +prompt();
+
+// console.log(typeof a);
+
+
+// if (typeof a == 'string') {
+//     console.log('a is string');
+// }
+
+// // if (a == NaN) {
+// if(isNaN(a)){
+//     console.log('is NaN')
+// }
+
+
+// ---------------------------------
+
+// a = -5;
+// if (a > 0) {
+//     console.log('a > 0');
+// } else {
+//     console.log('a < 0');
+// }
+
+
+// ---------------------------------
+
+variable = 10;
+
+if (variable > 0) {
+    console.log(' > 0');
+} else {
+    console.log(' < 0');
+}
+
+// if (''){console.log('yes')}
+// if (0){console.log('yes')}
+// if (undefined){console.log('yes')}
+// if (null){console.log('yes')}
+str = prompt();
+a = 10;
+if (!a || !str) {
+    console.error('WHAAAAT!');
+    // location.reload();
+}
