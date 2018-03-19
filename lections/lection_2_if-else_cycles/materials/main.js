40f96eef29ec949ffa08f22bf77723c310f51aa9
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-03-19 21:08:37 +0200

    L_2 update

--- lections/lection_2_if-else_cycles/materials/main.js
+++ lections/lection_2_if-else_cycles/materials/main.js
@@ -79,21 +79,86 @@ console.log("Lection_2");
 
 // ---------------------------------
 
-variable = 10;
+// variable = 10;
 
-if (variable > 0) {
-    console.log(' > 0');
-} else {
-    console.log(' < 0');
+// if (variable > 0) {
+//     console.log(' > 0');
+// } else {
+//     console.log(' < 0');
+// }
+
+// // if (''){console.log('yes')}
+// // if (0){console.log('yes')}
+// // if (undefined){console.log('yes')}
+// // if (null){console.log('yes')}
+// str = prompt();
+// a = 10;
+// if (!a || !str) {
+//     console.error('WHAAAAT!');
+//     // location.reload();
+// }
+
+// x = +prompt();
+
+// if (x < -5) {
+//     y = x * x;
+// } else if (x >= -5 && x <= 3) {
+//     y = x - 2;
+// } else {
+//     y = (x-2) * x;
+// }
+
+
+
+// ---------------------------------
+
+
+// цикл с предусловием
+// i = 1;
+// while (i < 10) { // условие выполнения цикла
+//     // тело
+//     i = i + 1;
+//     console.log(i);
+    
+// }
+// console.log('last', i);
+// // цикл с пост условием
+
+// i = 1;
+// do { 
+//     // тело
+
+//     i = i + 1;
+//     console.log(i);
+    
+// } while (i < 10);// условие выполнения цикла
+
+// console.log('last', i);
+
+
+// do { 
+//     a = +prompt();
+    
+// } while (isNaN(a) || a < 0);// условие выполнения цикла
+
+
+
+// for (i = 1, sum = 0; i <= 10; sum = sum + i, i++){
+//     console.log(i);
+
+//     // sum = sum + i;
+// }
+
+// console.log(sum)
+
+
+a = 5;
+n = 3;
+res = 1;
+for(i = 1; i <= n; i++){
+    res = res * a;
 }
 
-// if (''){console.log('yes')}
-// if (0){console.log('yes')}
-// if (undefined){console.log('yes')}
-// if (null){console.log('yes')}
-str = prompt();
-a = 10;
-if (!a || !str) {
-    console.error('WHAAAAT!');
-    // location.reload();
-}
+console.log(res);
+
+
