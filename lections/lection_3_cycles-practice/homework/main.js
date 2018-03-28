1109f891874bfdb81b1e28c95f6e5b1306f29d1b
lenka-zelenka <elenazologina@gmail.com>
2018-03-29 01:55:55 +0300

    bad romb

--- lections/lection_3_cycles-practice/homework/main.js
+++ lections/lection_3_cycles-practice/homework/main.js
@@ -58,26 +58,98 @@ document.write('<br/>');
 m = 11;
 star = '*';
 space = '';
-last = '**';
-middle = parseInt(m/2)
+last = '****';
+middle = parseInt(m/2);
+k=0;
+
 document.write('<br/>равносторонний треугольник<br />');
 
-// for (j=0; j < middle; j++){
-//     for (i = 0; i < m; i++) {
-//         document.write('&nbsp;');
-//         if(j == middle){
-//             document.write('*');
-//             middle--;
-            
-//         } else {
-//             space += '&nbsp;'
-//             document.write(space);
-//             document.write('*<br />');
-//         }
-    
-//     }
-// }
+for (j=1; j <= m; j++){
+    for (i = 1; i <= m; i++) {
+        document.write('&nbsp');
+        // рисуем среднюю звезду и уменьшаем среднее на один
+        if(j == 1 && i == middle) {
+            document.write(star);
+            middle--;
+        }
+        if(i == middle) {
+            document.write(star);
+            middle--;
+            space += '&nbsp'
+            k++;
+            last += star
+        }
+        if(i == middle + k ) {
+            document.write(space);
+            document.write(star);
 
-// document.write('*<br />');
+        }
+        if (middle == 0) {
+            document.write(last)
+            break
+         }
+    }
+    if (middle == 0) {
+        break
+     }
+    document.write('<br />');
+}
+
+document.write('<br /> <br />');
+
+
 
 // ромб 
+
+document.write('ромб: <br />');
+
+m = 10;
+star = '*';
+space = '.';
+last = '****';
+middle = parseInt(m/2);
+
+k=middle 
+
+for (i=0; i <= m; i++){
+    for (j = 0; j <= m; j++) {
+        document.write('.');
+        if(i == 0 && j == middle) {
+            // рисуем среднюю звезду и уменьшаем среднее на один
+            document.write(star);
+            middle--;
+        }
+        if(j == middle) {
+            document.write(star);
+            document.write(space);
+            document.write(star);
+            space += '..'
+            middle--;
+            break
+        }
+    }
+    document.write('<br />');
+    if(middle == 0){
+        break
+    }
+}
+
+document.write('<br/>');
+
+mid = 6;
+k=mid;
+for (i=0; i<m; i++){
+    for (j=0; j<m; j++){
+        document.write('.')        
+        // document.write( 'i'+i + 'j'+ j + ' ')
+        if(j == mid){
+            document.write('*')
+            mid--;
+            k++;
+        } 
+        if(j == k) {
+            document.write('*')
+        }
+    }
+    document.write('<br/>')
+}
