1c9e08f2b5c1b52259bf4365b3d3b0a641892a48
elenazologina <elenazologina@gmail.com>
2018-03-28 20:19:36 +0300

    3 hw

--- lections/lection_3_cycles-practice/homework/main.js
+++ lections/lection_3_cycles-practice/homework/main.js
@@ -55,62 +55,29 @@ document.write('<br/>');
 
 // равносторонний треугольник
 
-// m = 11;
-// star = '*';
-// space = '';
-// last = '**';
-// middle = parseInt(m/2)
-// document.write('<br/>равносторонний треугольник<br />');
+m = 11;
+star = '*';
+space = '';
+last = '**';
+middle = parseInt(m/2)
+document.write('<br/>равносторонний треугольник<br />');
+
+// for (j=0; j < middle; j++){
+//     for (i = 0; i < m; i++) {
+//         document.write('&nbsp;');
+//         if(j == middle){
+//             document.write('*');
+//             middle--;
+            
+//         } else {
+//             space += '&nbsp;'
+//             document.write(space);
+//             document.write('*<br />');
+//         }
+    
+//     }
+// }
 
 // document.write('*<br />');
-// for (i = 1; i < middle; i++) {
-//     document.write('*');
-//     document.write(space);
-//     document.write('*<br />');
-//     space += '&nbsp;'
-// }
-// // херь какая-то
-// for (i = middle; i > 0; i--) {
-//     document.write('*');
-//     document.write(space);
-   
-//     document.write('*<br />');
-//     // space += '&nbsp;'
-// }
 
-
-// 2. a и b вводятся с клавиатуры (числа). 
-// Каждое число от a до b вывести в квадрате с сохранением знака.
-
-//         -3, -2, -1, 0, 1, 2 
-//             -> 
-//         -9, -4, -1, 0, 1, 4 
-document.write('a и b вводятся с клавиатуры (числа).<br/>Каждое число от a до b вывести в квадрате с сохранением знака.<br/>')
-
-
-pow = '';
-do {
-    a = +prompt('Введите число а:')
-} while( isNaN(a) )
-do {
-    b = +prompt('Введите число b:')
-} while( isNaN(a) )
-
-if(a < b){
-    for(i = a; i <= b; i++){
-        i<0 ? document.write(-i*i + ', ') : document.write(i*i + ', ') ;
-        
-    }
-} else {
-    for(i = a; i <= b; i--){
-        i>0 ? document.write(i*i + ', ') : document.write(-i*i + ', ') ;        
-    }
-}
-
-
-// 3.  Дано `a = 1, b = 15;`. Вывести на экран таблицу, которая будет состоять из 2-ух рядков.
-//     В 1-ом рядке - нечетные числа: `13579111315`, в 2-ом - четные: `2468101214`
-
-// 4.  Ввести с клавиатуры числа `x, y` (заставить пользователя ввести числа). 
-//     Запустить цикл перебора от `x` до `y` (вне зависимости от значений)!. 
-//     Посчитать сумму парных чисел если `x > y`, непарных если `x < y`;
+// ромб 
