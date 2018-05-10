a9627b5c4037496c24c8fa518305700b3230b2d1
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-05-10 20:02:55 +0300

    L_14

---
+++ lections/lection_14_DOM_events/homework/README.md
@@ -0,0 +1,15 @@
+# Изучить материал
+
++ https://learn.javascript.ru/browser-environment
++ https://learn.javascript.ru/dom-nodes
++ https://learn.javascript.ru/dom-console
++ https://learn.javascript.ru/searching-elements-dom
++ https://learn.javascript.ru/basic-dom-node-properties
+
+# Практика
+
+    1) Создать простейшую анимацию падения блока. Вверху окна мы имеем 3 блока, при нажатии на один из блоков - он плавно падает вниз окна. Остается внизу.
+
+    2) При нажатии на блок который находится внизу - он плавно поднимается вверх.
+
+    3) Добавить поле ввода для параметра speed, которое влияет на скорость анимации блоков из задания 1 и 2.
---
+++ lections/lection_14_DOM_events/materials/main.js
@@ -0,0 +1,88 @@
+console.log('Lection_14');
+
+window.onload = function() {
+
+    var block = document.querySelector('.block'),
+        input = document.querySelector('.input');
+
+    // DOM level 0
+    // block.onclick = onBtnClick;
+    // block.onmouseover = onMouseOver;
+    // block.onmouseout = onMouseOut;
+    // block.onmousemove = onMouseMove;
+
+    // block.onclick = function(){
+    //     console.log('function 2');
+    // };
+
+    // setTimeout(function(){
+    //     block.onclick = null; // отмена события
+    // }, 5000);
+
+
+    // DOM level 2
+    // block.addEventListener('click', onBtnClick);
+    // block.addEventListener('click', function(){
+    //     console.log('function 2');
+    // });
+
+    
+    // setTimeout(function(){
+    //     block.removeEventListener('click', onBtnClick);
+    // }, 2000);
+
+    // mouseup, down, click
+
+    // input.addEventListener('mousedown', function(event){
+    //     console.log('mousedown');
+    // });
+
+    // input.addEventListener('mouseup', function(event){
+    //     console.log('mouseup');
+    // });
+
+    // ----------------------------------------------- form events
+
+    input.addEventListener('focus', function(event){
+        console.log('focus');
+
+        // this.value = "";
+    });
+
+    // input.addEventListener('input', function(event){
+    //     console.log('input', event);
+    // });
+
+    // input.addEventListener('change', function(event){
+    //     console.log('change', event);
+    // });
+
+    // input.addEventListener('blur', function(event){
+    //     console.log('blur', event);
+    // });
+
+    // input.addEventListener('click', function(event){
+    //     console.log('click');
+    // });
+}
+
+
+function onBtnClick(event) { // event - объект события
+    console.log('click', event);
+
+    this.style.backgroundColor = 'red';
+
+    // event.target.style.backgroundColor = 'red';
+}
+
+function onMouseOver(event) {
+    console.log('onMouseOver');
+}
+
+function onMouseOut(event) {
+    console.log('onMouseOut', event);
+}
+
+function onMouseMove(event) {
+    console.log('onMouseMove');
+}
